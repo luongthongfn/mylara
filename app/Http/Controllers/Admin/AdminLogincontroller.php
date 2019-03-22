c0a7b9d911a5ecd4c257b0a0a9380b77cf39e620
thongwav <luongthongfn@gmail.com>
2019-03-22 17:33:50 +0700

    update

--- app/Http/Controllers/Admin/AdminLogincontroller.php
+++ app/Http/Controllers/Admin/AdminLogincontroller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 class AdminLoginController extends Controller
 {
@@ -32,54 +33,61 @@ class AdminLoginController extends Controller
      *
      * @return void
      */
+
+    protected function guard()
+    {
+        return Auth::guard('admins');
+    }
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
 
+    //custom guard
     public function getLogin()
     {
         return view('admin/login');
     }
 
-    // public function postLogin(Request $request)
-    // {
-    //     $this->validateLogin($request);
-
-    //     // If the class is using the ThrottlesLogins trait, we can automatically throttle
-    //     // the login attempts for this application. We'll key this by the username and
-    //     // the IP address of the client making these requests into this application.
-    //     if ($this->hasTooManyLoginAttempts($request)) {
-    //         $this->fireLockoutEvent($request);
-
-    //         return $this->sendLockoutResponse($request);
-    //     }
-
-    //     if ($this->attemptLogin($request)) {
-    //         return $this->sendLoginResponse($request);
-    //     }
-
-    //     // If the login attempt was unsuccessful we will increment the number of attempts
-    //     // to login and redirect the user back to the login form. Of course, when this
-    //     // user surpasses their maximum number of attempts they will get locked out.
-    //     $this->incrementLoginAttempts($request);
-
-    //     return $this->sendFailedLoginResponse($request);
-    // }
-
-    public function postLogin( Request $request)
+    public function postLogin(Request $request)
     {
-        // dd($request);
-        if (auth()->guard('admins')->attempt([
-            'email'    => $request->email,
-            'password' => $request->password
-        ])) {
-            // echo 'pass';
-            dd(auth()->guard('admins')->user());
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
         }
 
-        return back()->withErrors(['email' => 'Email or password are wrong.']);
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
+
+    // public function postLogin( Request $request)
+    // {
+    //     // dd($request);
+    //     if (auth()->guard('admins')->attempt([
+    //         'email'    => $request->email,
+    //         'password' => $request->password
+    //     ])) {
+    //         // echo 'pass';
+    //         dd(auth()->guard('admins')->user()->toArray());
+    //     }
+
+    //     return back()->withErrors(['email' => 'Email or password are wrong.']);
+    // }
 
 
 }
--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -39,19 +39,10 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin/login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin/register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
+                        @auth('admins')
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ auth()->guard('admins')->user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
@@ -66,7 +57,17 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                        @else
+
+                            <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin/login') }}">{{ __('Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('admin/register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                        @endauth
                     </ul>
                 </div>
             </div>
