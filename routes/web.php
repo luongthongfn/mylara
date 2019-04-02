c5b0989e4b7a4cfa2a92e402afddfa2d33e5ac24
thongwav <luongthongfn@gmail.com>
2019-04-02 13:59:24 +0700

    feat(#wip): add admin theme

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,33 @@ Route::get('/info', 'Mycontroller@info');
 
 // view================================================
 
+Route::get('/get', function () {
+
+    $id = '58IkVCh3hWU';
+    $dt = file_get_contents("http://www.youtube.com/get_video_info?video_id=$id&el=embedded&ps=default&eurl=&gl=US&hl=en");
+    $x = explode("&", $dt);
+    $t = array(); $g = array(); $h = array();
+    echo "<pre>rn";
+    foreach ($x as $r) {
+        $c = explode("=", $r);
+        $n = $c[0]; $v = $c[1];
+        $y = urldecode($v);
+        $t[$n] = $v;
+    }
+    $streams = explode(',', urldecode($t['url_encoded_fmt_stream_map']));
+    foreach ($streams as $dt) {
+        $x = explode("&", $dt);
+        foreach ($x as $r) {
+            $c = explode("=", $r);
+            $n = $c[0]; $v = $c[1];
+            $h[$n] = urldecode($v);
+        }
+        $g[] = $h;
+    }
+    print_r($g);
+    echo "rn</pre>";
+});
+
 Route::get('/view', function () {
     return view('layout/master');
 });
@@ -33,7 +60,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 // --------------------------------------------------------
 // --------------------------------------------------------
 
-
 Route::post('/logout', 'Admin\Auth\AdminLoginController@logout')->name('logout');
 Route::group(['prefix' => '/admin'], function () {
     Route::get('/login', 'Admin\Auth\AdminLoginController@showLoginForm')->name('admin/login');
@@ -42,9 +68,6 @@ Route::group(['prefix' => '/admin'], function () {
     Route::get('/register', 'Admin\Auth\AdminRegisterController@showRegistrationForm')->name('admin/register');;
     Route::post('/postRegister', 'Admin\Auth\AdminRegisterController@register')->name('admin/postRegister');
 
-
     Route::get('/', 'Admin\AdminController@index');
     Route::get('/home', 'Admin\AdminController@index');
 });
-
-
